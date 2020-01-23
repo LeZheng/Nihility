efaa2f3b6a488d4dd1fc450b4f23f13e781ab4e2
LeZheng <460163461@qq.com>
2020-01-23 12:32:49 +0800

    init

---
+++ Nihility/Nihility_zh_CN.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Nihility_zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="8"/>
+        <source>Nihility</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="14"/>
+        <source>Small</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="17"/>
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="21"/>
+        <source>Large</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
